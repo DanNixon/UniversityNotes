1179d64f58da2b4326c17e72a8cae1fb14f788fd
Dan Nixon <dan@dan-nixon.com>
2015-12-31 12:20:32 +0000

    Some programming for games notes

---
+++ listings/polymorphism_1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+class ITouhou {
+ public:
+  ITouhou() { cout << "ITouhou constructed" << endl; }
+  virtual ~ITouhou() { cout << "ITouhou destructed" << endl; }
+  virtual string name() const = 0;
+};
+
+class Youkai : public ITouhou {
+ public:
+  Youkai() : ITouhou() { cout << "Youkai constructed" << endl; }
+  virtual ~Youkai() { cout << "Youkai destructed" << endl; }
+};
+
+class IFlyable {
+ public:
+  IFlyable() { cout << "IFlyable constructed" << endl; }
+  ~IFlyable() { cout << "IFlyable destructed" << endl; }
+  virtual void fly() { cout << "IFlyable::fly()" << endl; }
+};
+
+class Yuuka : public Youkai, public IFlyable {
+ public:
+  Yuuka() : Youkai(), IFlyable() { cout << "Yuuka constructed" << endl; }
+  ~Yuuka() { cout << "Yuuka destructed" << endl; }
+  string name() const { return "Yuuka"; }
+  void fly() { cout << "Yuuka::fly()" << endl; }
+};
+
+int main() {
+  ITouhou *y1 = new Yuuka();
+  delete y1;
+
+  cout << "=====" << endl;
+
+  Youkai *y2 = new Yuuka();
+  delete y2;
+
+  cout << "=====" << endl;
+
+  Yuuka *y3 = new Yuuka();
+  y3->fly();
+  delete y3;
+
+  cout << "=====" << endl;
+
+  IFlyable *y4 = new Yuuka();
+  y4->fly();
+
+  return 0;
+}
