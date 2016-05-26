6baccb9effd0fa3db20124c7bf2741b0265d62bd
Dan Nixon <dan@dan-nixon.com>
2016-05-26 15:29:20 +0100

    add Travis build and publishing

---
+++ .travis_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Don't publish for a pull request or a non-master branch
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ];
+then
+  echo Not publishing PDFs.
+  exit 0;
+fi
+
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+cd out
+git init
+
+git add CSC*.pdf
+git commit -m "Deploy PDFs"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
